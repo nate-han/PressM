47aed34db3410d23982c6b96fc5000637f81d0c6
nate-han <4233364+nate-han@users.noreply.github.com>
2018-03-16 22:14:22 +0000

    Now with GUI
    built with WarMenu

--- PressM_AIO/client.lua
+++ PressM_AIO/client.lua
@@ -11,16 +11,30 @@ function showNote(msg)
 	
 end
 
-function setHealth(who)
-
-	SetEntityInvincible(who, 1)
-	
-end
-
 function giveWeapon(who, what, amount)
 
 	showNote("Giving: ~g~" .. amount .. " of ~r~" .. what)
 	GiveWeaponToPed(who, GetHashKey(what), amount, false)
+	
+end
+
+function setHealth(who, state)
+	
+	local Invincible = state
+	
+	if Invincible then
+	
+		SetEntityInvincible(who, 0)
+		showNote("Godmode: ~r~OFF")
+		return false
+		
+	else
+		
+		SetEntityInvincible(who, 1)
+		showNote("Godmode: ~g~ON")
+		return true
+		
+	end
 	
 end
 
@@ -34,29 +48,80 @@ function getCoords(who)
 	
 end
 
-AddEventHandler("playerSpawned", function(spawn)
-	
-	local target = GetPlayerPed(-1)
-	
-	setHealth(target)
-	
-end)
-
 Citizen.CreateThread(function()
 
+    local ammo = { "WEAPON_PISTOL", "GADGET_PARACHUTE"}
+    local currentItemIndex = 1
+    local selectedItemIndex = 1
+    local checkbox = true
+	local target = GetPlayerPed(-1)
+
+    WarMenu.CreateMenu('PressM', 'PressM_AIO')
+	WarMenu.CreateSubMenu('Weapons', 'PressM', 'Choose Your Weapon')
+	
 	while true do
 	
+		-- MAIN MENU
+	
+		if WarMenu.IsMenuOpened("PressM") then
+		
+			if WarMenu.MenuButton('Weapons', 'Weapons') then
+				
+				
+			elseif WarMenu.Button('Get Coordinates') then
+		
+				getCoords(GetPlayerPed(-1))
+				
+			elseif WarMenu.Button("Exit") then
+			
+				WarMenu.CloseMenu()
+				
+			end
+			
+			WarMenu.Display()
+			
+		-- WEAPONS MENU
+			
+		elseif WarMenu.IsMenuOpened("Weapons") then
+			
+			if WarMenu.Button("Pistol") then
+				
+				giveWeapon(target, "WEAPON_PISTOL", 100, false)
+				
+			elseif WarMenu.Button('Parachute') then
+				
+				giveWeapon(target, "GADGET_PARACHUTE", 1, false)
+				
+			elseif WarMenu.Button('SMG') then
+				
+				giveWeapon(target, "WEAPON_MICROSMG", 100, false)
+				
+			elseif WarMenu.Button('Assault Rifle') then
+				
+				giveWeapon(target, "WEAPON_ASSAULTRIFLE", 100, false)
+				
+			elseif WarMenu.Button('Sniper Rifle') then
+				
+				giveWeapon(target, "WEAPON_HEAVYSNIPER", 100, false)
+				
+			elseif WarMenu.Button("Exit") then
+			
+				WarMenu.CloseMenu()
+				
+			end
+				
+			WarMenu.Display()
+			
+		-- OPEN MENU
+		
+		elseif IsControlJustReleased(0, 244) then
+		
+			WarMenu.OpenMenu("PressM")
+		
+		end
+		
 		Citizen.Wait(0)
 		
-		if(IsControlJustPressed(0, 244)) then
-			
-			local target = GetPlayerPed(-1)
-		
-			giveWeapon(target, "WEAPON_PISTOL", 100, false)
-			giveWeapon(target, "GADGET_PARACHUTE", 1, false)
-			getCoords(target)
-			
-		end
 	end
 	
 end)
